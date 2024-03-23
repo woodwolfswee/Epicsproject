60107ca1fff6afe586b5fd24f42455e72f0fb72a
woodwolfswee <shreyan01.dhar@gmail.com>
2024-03-23 11:29:16 +0530

    Added my project

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:epics_project1/pages/Home_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:lottie/lottie.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
@@ -34,14 +35,13 @@ class _SplashScreenState extends State<SplashScreen> {
 
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
     Future.delayed(const Duration(seconds: 5)).then((value) => Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context)=> HomePage())));
   }
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Container(
+        child: SizedBox(
           height: 200.0,
           width: 200.0,
           child: LottieBuilder.asset('assets/animations/mapanimation.json'),
